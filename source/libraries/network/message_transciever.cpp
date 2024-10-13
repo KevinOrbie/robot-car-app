a619f51af9bc1fa9e2e5a752e3a099bc225ed1a5
KevinOrbie <kevin.orbie@gmail.com>
2024-10-13 16:23:56 +0200

    refactor: removed depricated code

--- source/libraries/network/message_transciever.cpp
+++ source/libraries/network/message_transciever.cpp
@@ -21,104 +21,7 @@
 
 
 namespace message {
-/* ========================== Classes ========================== */
-
-void Transciever::send(std::unique_ptr<message::MessageBase> msg) {
-    /* No message given? */
-    if (!msg) {
-        /* Pop message from send Queue. */
-        msg = popSendQueue();
-
-        /* Return early if there is no message to send. */
-        if (!msg) { return; }
-    }
-
-    /* Preliminary Checks. */
-    if (!connection_.valid()) {
-        LOGW("Message not sent: Invalid Connection! (Could be because it is not yet initialized)");
-        return;
-    }
-
-    /* Send Header. */
-    sendMessageID(msg->getID());
-    
-    /* Send Payload. */
-    msg->serialize(connection_);
-};
-
-void Transciever::recieve() {
-    /* Preliminary Checks. */
-    if (!connection_.valid()) {
-        LOGW("Message not recieved: Invalid Connection! (Could be because it is not yet initialized)");
-        return;
-    }
-
-    /* Recieve Header. */
-    MessageID id = recieveMessageID();
-    
-    /* Ignore if nothing was recieved. */
-    if (id != MessageID::EMPTY) { 
-        /* Recieve Message. */
-        std::unique_ptr<message::MessageBase> msg = MessageBase::deserialize(id, connection_);
-        pushRecieveQueue(std::move(msg));
-    }
-};
-
-
-void Transciever::sendMessageID(MessageID id) {
-    LOGI("Sending MessageID: %d", static_cast<int>(id));
-    int message_id = static_cast<int>(id);
-    char* type_buffer = reinterpret_cast<char*>(&message_id);
-    int num_bytes = sizeof(int);
-    connection_.send(type_buffer, num_bytes);
-}
-
-MessageID Transciever::recieveMessageID() {
-    int message_id = -1;
-
-    /* Recieve the MessageID as an interger. */
-    bool message_available = connection_.recieve(reinterpret_cast<char*>(&message_id), sizeof(int));
-
-    /* Check if something was recieved. */
-    if(!message_available) {
-        return MessageID::EMPTY;
-    };
-
-    /* Find the corresponding MessageID. */
-    MessageID id = static_cast<MessageID>(message_id);
-    return id;
-}
-
-void Transciever::pushSendQueue(std::unique_ptr<message::MessageBase> msg) {
-    if (!msg) { LOGW("Don't push nullpointers to the send queue."); return; }
-    std::lock_guard lock(send_queue_mutex_);
-    send_queue_.push_back(std::move(msg));
-};
-
-void Transciever::pushRecieveQueue(std::unique_ptr<message::MessageBase> msg) {
-    if (!msg) { LOGW("Don't push nullpointers to the recieve queue."); return; }
-    std::lock_guard lock(recieved_queue_mutex_);
-    recieved_queue_.push_back(std::move(msg));
-};
-
-std::unique_ptr<message::MessageBase> Transciever::popSendQueue() {
-    std::lock_guard lock(send_queue_mutex_);
-    if (send_queue_.empty()) { return nullptr; }
-    std::unique_ptr<message::MessageBase> msg = std::move(send_queue_.front());
-    send_queue_.pop_front();
-    return msg;
-};
-
-std::unique_ptr<message::MessageBase> Transciever::popRecieveQueue() {
-    std::lock_guard lock(recieved_queue_mutex_);
-    if (recieved_queue_.empty()) { return nullptr; }
-    std::unique_ptr<message::MessageBase> msg = std::move(recieved_queue_.front());
-    recieved_queue_.pop_front();
-    return msg;
-};
-
-
-// ########################### TRANSMITTER ###########################
+/* ============================== Transmitter ============================== */
 void Transmitter::iteration() {
     /* If ran in seperate thread, make this thread block, while waiting for available message. */
     if (threaded()) {
@@ -203,7 +106,7 @@ int Transmitter::getQueueSize() {
 }
 
 
-// ############################ RECIEVER #############################
+/* =============================== Reciever ================================ */
 void Reciever::iteration() {
     /* If ran in seperate thread, make this thread block, while waiting to recieve a message. */
     if (threaded()) {
--- source/libraries/network/message_transciever.h
+++ source/libraries/network/message_transciever.h
@@ -23,39 +23,6 @@
 
 namespace message {
 /* ========================== Classes ========================== */
-
-class Transciever {
-   public:
-    /**
-     * @brief Send a single message.
-     */
-    void send(std::unique_ptr<message::MessageBase> msg=nullptr);
-
-    /**
-     * @brief Recieve a single message.
-     */
-    void recieve();
-
-    void pushSendQueue(std::unique_ptr<message::MessageBase> msg);
-    std::unique_ptr<message::MessageBase> popRecieveQueue();
-
-   protected:
-    void sendMessageID(MessageID id);
-    MessageID recieveMessageID();
-
-    std::unique_ptr<message::MessageBase> popSendQueue();
-    void pushRecieveQueue(std::unique_ptr<message::MessageBase> msg);
-
-   protected:
-    std::deque<std::unique_ptr<message::MessageBase>> send_queue_;
-    std::mutex send_queue_mutex_;
-    std::deque<std::unique_ptr<message::MessageBase>> recieved_queue_;
-    std::mutex recieved_queue_mutex_;
-
-    Connection connection_;
-};
-
-
 class Transmitter: public Looper {
    public:
     Transmitter(Connection* connection=nullptr): connection_(connection) {};
